b103d16d60c32e92b2267df1c6f805262cb650d8
Jeremy <jeremy.pouyet@epitech.eu>
2016-02-06 12:53:10 +0000

    more comments

--- ProblemLoader.hh
+++ ProblemLoader.hh
@@ -11,13 +11,31 @@ public:
     ProblemLoader();
     ~ProblemLoader();
     
+    /**
+     * load a library into the libraryLoader
+     * @param libPath, shared library path
+     * @return the loaded problem. Throw an error if the function encounters an issue. 
+     */
     Problem *load(const std::string &libPath);
+    
+    /**
+     * CLose the current library and delete the current problem
+     */
     void    close();
+    
+    /**
+     * get the current problem
+     * @return the current problem or NULL if not problem is loaded
+     */
     Problem *getProblem() const;
 private:
+    // pointer to a shared library entrance
     void        *_sharedLibrary;
+    // pointer to the current problem
     Problem     *_problem;
+    // Pointer to the Problem constructor
     create_t    *_constructor;
+    // Pointer to the Problem destructor
     destroy_t   *_destructor;
 };
 
