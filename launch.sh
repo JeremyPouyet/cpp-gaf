348d57e34748820c85c14e2434689aadce34ba34
Jeremy Pouyet <jeremy.pouyet@epitech.eu>
2016-02-20 15:45:54 +0000

    better test launcher

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cmd="time ./open-gaf problems/iis/Iis.so problems/iis/problem.ini && gnuplot -p conf.gnp"
+cmd="time ./open-gaf problems/iis/ && gnuplot -p conf.gnp"
 if [ "$#" -ne 0 ]; then
     make re && make -C problems/iis/ && eval $cmd
 else
-    time ./open-gaf problems/iis/ && gnuplot -p conf.gnp
+    eval $cmd
 fi
